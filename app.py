d5ee453d02fdd234f956d46020bae6cb13dc1c54
Rachel Chua <65063550+rachel45605@users.noreply.github.com>
2024-08-14 16:36:46 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route("/", methods= ["GET","POST"])
-def indec():
+def index():
     return(render_template("index.html"))
 
 if __name__ == "__main__":
